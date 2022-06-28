25a0c117a29df89184ee42e97006111772ddca95
Mabel <mookonkw@users.noreply.github.com>
2022-06-28 22:23:17 +0000

    day 2 python

---
+++ pike/trikfoot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+     pike_trikfoot = {'Race' : 'Gnome' , 'Team' : ' Vox Machina', 'Team_Members' : '["Grog", "Keyleth", "Scanlan", "VexAhlia" , "VaxIldan", "Percy"]', 'Bestfriend' : 'Grog' , 'age' : 40 }
+
+     print(pike_trikfoot)
+main()
+
+
+
+
