8b07dd73a5663daa1203597ab3162331ace587c3
Mabel <mookonkw@users.noreply.github.com>
2022-07-06 15:53:42 +0000

    API's

--- copycat01.py
+++ copycat01.py
@@ -11,5 +11,7 @@ os.chdir("/home/student/mycode/")
 shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
 
 # copy the entire directoryA to directoryB
+os.system('rm -rf /home/student/mycode/5g_research_backup/"
 shutil.copytree("5g_research/", "5g_research_backup/")
+
 
